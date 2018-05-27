d9b2bb87771c68ef9524a401bd93000f3ea8e88b
waterbeast <csrane@gmail.com>
2018-05-28 01:02:12 +0900

    accounts 앱 생성 및 추가

--- photowall/urls.py
+++ photowall/urls.py
@@ -20,7 +20,9 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls')),
     url(r'^blog/', include('blog.urls' , namespace='blog')),
+    
 
 ]
 
